caf453d29820189bc914f7de94d860b48f7ba590
Dulat <ropdxm@gmail.com>
2024-02-15 20:31:58 +0600

    near blue auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.tuning;
 
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -23,14 +26,51 @@ public final class SplineTest extends LinearOpMode {
 //                        .splineTo(new Vector2d(-30, -30), Math.PI / 2)
 //                        .build());
 
-            Actions.runBlocking(
-                    drive.actionBuilder(new Pose2d(12, 60, Math.toRadians(270)))
+            Action trajStart = drive.actionBuilder(new Pose2d(12, 60, Math.toRadians(270)))
+                    .lineToY(34)
+//                .waitSeconds(0.4)
+//                .turn(Math.toRadians(-90))
+//                .lineToX(50)
+                    .build();
+            int propPosition = 2;
+            TrajectoryActionBuilder trajPropBuilder = drive.
+                    actionBuilder(new Pose2d(12, 34, Math.toRadians(270)));
+            Action trajProp, reverseTrajProp;
+            switch(propPosition){
+                case 0: // left
+                    trajProp = trajPropBuilder.turn(-Math.PI/2).build();
+                    reverseTrajProp = drive.
+                            actionBuilder(new Pose2d(12, 34, Math.toRadians(180))).
+                            turn(Math.toRadians(-90)).build();
+                    break;
+                case 1: // center
+                    trajProp = trajPropBuilder.turn(Math.PI).build();
+                    reverseTrajProp = drive.
+                            actionBuilder(new Pose2d(12, 34, Math.toRadians(90)))
+                            .turn(Math.toRadians(0)).build();
+                    break;
+                case 2: // right
+                    trajProp = trajPropBuilder.turn(Math.PI/2).build();
+                    reverseTrajProp = drive.
+                            actionBuilder(new Pose2d(12, 34, 0.0)).
+                            turn(-Math.toRadians(-90)).build();
+                    break;
+                default:
+                    throw new Error("Unknown team prop position");
+            }
+            Action trajGoToBackdrop = drive.actionBuilder(new Pose2d(12, 34, Math.toRadians(90)))
+                    .splineTo(new Vector2d(25, 47), 0)
+                    .splineTo(new Vector2d(50, 33), 0)
+                    .build();
+            Actions.runAction(new SequentialAction(
+                    trajStart,
+                    trajProp,
 
-                            .lineToY(34)
-                            .waitSeconds(0.4)
-                            .turn(Math.toRadians(-90))
-                            .lineToX(50)
-                            .build());
+                    // servo action
+                    reverseTrajProp,
+                    trajGoToBackdrop
+            ));
+
 
         } else if (TuningOpModes.DRIVE_CLASS.equals(TankDrive.class)) {
             TankDrive drive = new TankDrive(hardwareMap, beginPose);

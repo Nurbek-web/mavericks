f4c385a12ae7d5dc55924cb0800d1eda14baf64f
Dulat <ropdxm@gmail.com>
2024-02-17 00:03:37 +0600

    blue auto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -17,7 +17,7 @@ enum Route {
 }
 
 public class MeepMeepTesting {
-    public static final Route ROUTE = Route.RED_NEAR;
+    public static final Route ROUTE = Route.BLUE_FAR;
 
     public static final double DELAY = 0.5;
     public static final double MAX_VEL = 60;
@@ -34,15 +34,18 @@ public class MeepMeepTesting {
 
 
         switch (ROUTE) {
+            case BLUE_NEAR:
+                myBot = blueNear(meepMeep);
+                break;
+            case BLUE_FAR:
+                myBot = blueFar(meepMeep);
+                break;
             case RED_NEAR:
                 myBot = redNear(meepMeep);
                 break;
-//            case RED_FAR:
-//                break;
-//            case BLUE_NEAR:
-//                break;
-//            case BLUE_FAR:
-//                break;
+            case RED_FAR:
+                myBot = redFar(meepMeep);
+                break;
 
             default:
                 throw new Error("Initializition problem");
@@ -67,50 +70,187 @@ public class MeepMeepTesting {
                 .start();
     }
 
+    private static RoadRunnerBotEntity blueNear(MeepMeep meepMeep) {
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setConstraints(MAX_VEL, MAX_ACCEL, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        TrajectoryActionBuilder trajBackdrop, trajStart;
+        int propPosition = 2;
+        switch(propPosition){
+            case 0: // right
+                trajStart = myBot.getDrive()
+                        .actionBuilder(new Pose2d(12, 60, Math.toRadians(90)))
+                        .strafeToLinearHeading(new Vector2d(10, 34), 0).waitSeconds(1);
+                trajBackdrop = myBot.getDrive().actionBuilder(new Pose2d(10, 34, 0))
+                        .strafeToLinearHeading(new Vector2d(48, 33), Math.toRadians(180)).waitSeconds(1);
+                break;
+            case 1: // center
+                trajStart = myBot.getDrive()
+                        .actionBuilder(new Pose2d(12, 60, Math.toRadians(90)))
+                        .strafeToConstantHeading(new Vector2d(10, 34)).waitSeconds(1);
+                trajBackdrop = myBot.getDrive().actionBuilder(new Pose2d(10, 34, Math.toRadians(90)))
+                        .strafeToConstantHeading(new Vector2d(20, 33)).strafeToLinearHeading(new Vector2d(48, 33), Math.toRadians(180)).waitSeconds(1);
+
+                break;
+            case 2: // left
+                trajStart = myBot.getDrive()
+                        .actionBuilder(new Pose2d(12, 60, Math.toRadians(90)))
+                        .strafeToLinearHeading(new Vector2d(14, 34), Math.toRadians(180)).waitSeconds(1);
+                trajBackdrop = myBot.getDrive().actionBuilder(new Pose2d(14, 34, Math.toRadians(180)))
+                        .strafeToLinearHeading(new Vector2d(20, 45), Math.toRadians(135)).strafeToLinearHeading(new Vector2d(48, 33), Math.toRadians(180)).waitSeconds(1);
+
+                break;
+            default:
+                throw new Error("Unknown team prop position");
+        }
+
+        myBot.runAction(new SequentialAction(
+                trajStart.build(),
+                trajBackdrop.build()
+        ));
+
+        return myBot;
+    }
+
+    private static RoadRunnerBotEntity blueFar(MeepMeep meepMeep) {
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setConstraints(MAX_VEL, MAX_ACCEL, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+
+        TrajectoryActionBuilder trajStart, trajBackdrop;
+        int propPosition = 2;
+
+        switch(propPosition){
+            case 0: // right
+                trajStart = myBot.getDrive().actionBuilder(new Pose2d(-34, 60, Math.toRadians(90)))
+                        .strafeToLinearHeading(new Vector2d(-36, 34), Math.toRadians(0));
+                trajBackdrop = myBot.getDrive().actionBuilder(new Pose2d(-36, 34, Math.toRadians(0)))
+                        .strafeToConstantHeading(new Vector2d(12, 34)).strafeToLinearHeading(new Vector2d(48, 33), Math.toRadians(180));
+                break;
+            case 1: // center
+                trajStart = myBot.getDrive().actionBuilder(new Pose2d(-34, 60, Math.toRadians(90)))
+                        .strafeToConstantHeading(new Vector2d(-34, 34));
+                trajBackdrop = myBot.getDrive().actionBuilder(new Pose2d(-34, 34, Math.toRadians(90)))
+                        .strafeToConstantHeading(new Vector2d(12, 34)).strafeToSplineHeading(new Vector2d(48, 33), Math.toRadians(180));
+                break;
+            case 2: // left
+                trajStart = myBot.getDrive().actionBuilder(new Pose2d(-34, 60, Math.toRadians(90)))
+                        .lineToY(46).strafeToLinearHeading(new Vector2d(-34, 30), Math.toRadians(180));
+                trajBackdrop = myBot.getDrive().actionBuilder(new Pose2d(-34, 30, Math.toRadians(180)))
+                        .strafeToConstantHeading(new Vector2d(-34, 58))
+                        .strafeToConstantHeading(new Vector2d(12, 58))
+                        .strafeToConstantHeading(new Vector2d(48, 33));
+                break;
+            default:
+                throw new Error("Unknown team prop position");
+        }
+
+        myBot.runAction(new SequentialAction(
+                trajStart.build(),
+                trajBackdrop.build()
+        ));
+
+        return myBot;
+    }
+
     private static RoadRunnerBotEntity redNear(MeepMeep meepMeep) {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 .setConstraints(MAX_VEL, MAX_ACCEL, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
 
-        Action trajStart = myBot.getDrive().actionBuilder(new Pose2d(12, 60, Math.toRadians(270)))
-                .lineToY(34)
+        Action trajStart = myBot.getDrive().actionBuilder(new Pose2d(12, -60, Math.toRadians(90)))
+                .lineToY(-34)
 //                .waitSeconds(0.4)
 //                .turn(Math.toRadians(-90))
 //                .lineToX(50)
                 .build();
         int propPosition = 2;
         TrajectoryActionBuilder trajPropBuilder = myBot.getDrive().
-                actionBuilder(new Pose2d(12, 34, Math.toRadians(270)));
+                actionBuilder(new Pose2d(12, -34, Math.toRadians(90)));
         Action trajProp, reverseTrajProp;
         switch(propPosition){
             case 0: // left
                 trajProp = trajPropBuilder.turn(-Math.PI/2).build();
                 reverseTrajProp = myBot.getDrive().
-                        actionBuilder(new Pose2d(12, 34, Math.toRadians(180))).
+                        actionBuilder(new Pose2d(12, -34, Math.toRadians(0))).
                         turn(Math.toRadians(-90)).build();
                 break;
             case 1: // center
                 trajProp = trajPropBuilder.turn(Math.PI).build();
                 reverseTrajProp = myBot.getDrive().
-                        actionBuilder(new Pose2d(12, 34, Math.toRadians(90)))
-                        .turn(Math.toRadians(0)).build();
+                        actionBuilder(new Pose2d(12, 34, Math.toRadians(270)))
+                        .build();
                 break;
             case 2: // right
                 trajProp = trajPropBuilder.turn(Math.PI/2).build();
                 reverseTrajProp = myBot.getDrive().
-                        actionBuilder(new Pose2d(12, 34, 0.0)).
-                        turn(-Math.toRadians(-90)).build();
+                        actionBuilder(new Pose2d(12, -34, Math.PI)).
+                        turn(Math.toRadians(90)).build();
                 break;
             default:
                 throw new Error("Unknown team prop position");
         }
-        Action trajGoToBackdrop = myBot.getDrive().actionBuilder(new Pose2d(12, 34, Math.toRadians(90)))
-                .splineTo(new Vector2d(25, 47), 0)
-                .splineTo(new Vector2d(50, 33), 0)
+        Action trajGoToBackdrop = myBot.getDrive().actionBuilder(new Pose2d(12, -34, Math.toRadians(270)))
+                .splineTo(new Vector2d(25, -47), 0)
+                .splineTo(new Vector2d(50, -33), 0)
                 .build();
         myBot.runAction(new SequentialAction(
-    trajStart,
+                trajStart,
+                trajProp,
+
+                // servo action
+                reverseTrajProp,
+                trajGoToBackdrop
+        ));
+
+        return myBot;
+    }
+
+    private static RoadRunnerBotEntity redFar(MeepMeep meepMeep) {
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setConstraints(MAX_VEL, MAX_ACCEL, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+
+        Action trajStart = myBot.getDrive().actionBuilder(new Pose2d(-34, -60, Math.toRadians(90)))
+                .lineToY(-34)
+                .build();
+        int propPosition = 2;
+        TrajectoryActionBuilder trajPropBuilder = myBot.getDrive().
+                actionBuilder(new Pose2d(-34, -34, Math.toRadians(90)));
+        Action trajProp, reverseTrajProp;
+        switch(propPosition){
+            case 0: // left
+                trajProp = trajPropBuilder.turn(-Math.PI/2).build();
+                reverseTrajProp = myBot.getDrive().
+                        actionBuilder(new Pose2d(-34, -34, Math.toRadians(0))).
+                        turn(Math.toRadians(-90)).build();
+                break;
+            case 1: // center
+                trajProp = trajPropBuilder.turn(Math.PI).build();
+                reverseTrajProp = myBot.getDrive().
+                        actionBuilder(new Pose2d(-34, -34, Math.toRadians(270)))
+                        .build();
+                break;
+            case 2: // right
+                trajProp = trajPropBuilder.turn(Math.PI/2).build();
+                reverseTrajProp = myBot.getDrive().
+                        actionBuilder(new Pose2d(-34, -34, Math.PI)).
+                        turn(Math.toRadians(90)).build();
+                break;
+            default:
+                throw new Error("Unknown team prop position");
+        }
+        Action trajGoToBackdrop = myBot.getDrive().actionBuilder(new Pose2d(-34, -34, Math.toRadians(270)))
+                .splineTo(new Vector2d(-28, -60), 0)
+                .lineToX(25)
+                .splineTo(new Vector2d(50, -33), 0)
+                .build();
+        myBot.runAction(new SequentialAction(
+                trajStart,
                 trajProp,
 
                 // servo action
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
@@ -14,7 +14,7 @@ import org.firstinspires.ftc.teamcode.TankDrive;
 public final class SplineTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(12, 60, Math.toRadians(270));
+        Pose2d beginPose = new Pose2d(12, 60, Math.toRadians(90));
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
             MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
 
@@ -26,43 +26,44 @@ public final class SplineTest extends LinearOpMode {
 //                        .splineTo(new Vector2d(-30, -30), Math.PI / 2)
 //                        .build());
 
-            Action trajStart = drive.actionBuilder(new Pose2d(12, 60, Math.toRadians(270)))
+            Action trajStart = drive.actionBuilder(new Pose2d(12, 60, Math.toRadians(90)))
                     .lineToY(34)
 //                .waitSeconds(0.4)
 //                .turn(Math.toRadians(-90))
 //                .lineToX(50)
                     .build();
-            int propPosition = 2;
+            int propPosition = 1;
             TrajectoryActionBuilder trajPropBuilder = drive.
-                    actionBuilder(new Pose2d(12, 34, Math.toRadians(270)));
+                    actionBuilder(new Pose2d(12, 34, Math.toRadians(90)));
             Action trajProp, reverseTrajProp;
             switch(propPosition){
                 case 0: // left
                     trajProp = trajPropBuilder.turn(-Math.PI/2).build();
                     reverseTrajProp = drive.
-                            actionBuilder(new Pose2d(12, 34, Math.toRadians(180))).
+                            actionBuilder(new Pose2d(12, 34, Math.toRadians(0))).
                             turn(Math.toRadians(-90)).build();
                     break;
                 case 1: // center
-                    trajProp = trajPropBuilder.turn(Math.PI).build();
+                    trajProp = trajPropBuilder.build();
                     reverseTrajProp = drive.
                             actionBuilder(new Pose2d(12, 34, Math.toRadians(90)))
-                            .turn(Math.toRadians(0)).build();
+//                        .turn(Math.toRadians(180))
+                            .build();
                     break;
                 case 2: // right
-                    trajProp = trajPropBuilder.turn(Math.PI/2).build();
+                    trajProp = trajPropBuilder.turn(Math.PI/2).waitSeconds(1).build();
                     reverseTrajProp = drive.
-                            actionBuilder(new Pose2d(12, 34, 0.0)).
-                            turn(-Math.toRadians(-90)).build();
+                            actionBuilder(new Pose2d(12, 34, 180)).waitSeconds(1).
+                            turn(Math.toRadians(90)).waitSeconds(1).build();
                     break;
                 default:
                     throw new Error("Unknown team prop position");
             }
             Action trajGoToBackdrop = drive.actionBuilder(new Pose2d(12, 34, Math.toRadians(90)))
-                    .splineTo(new Vector2d(25, 47), 0)
-                    .splineTo(new Vector2d(50, 33), 0)
+//                .splineTo(new Vector2d(25, 47), 180)
+                    .strafeToLinearHeading(new Vector2d(50, 33), Math.toRadians(180))
                     .build();
-            Actions.runAction(new SequentialAction(
+            Actions.runBlocking(new SequentialAction(
                     trajStart,
                     trajProp,
 
@@ -86,4 +87,198 @@ public final class SplineTest extends LinearOpMode {
             throw new RuntimeException();
         }
     }
+
+    private static void blueNear(MecanumDrive drive) {
+        Action trajStart = drive.actionBuilder(new Pose2d(12, 60, Math.toRadians(270)))
+                .lineToY(34)
+//                .waitSeconds(0.4)
+//                .turn(Math.toRadians(-90))
+//                .lineToX(50)
+                .build();
+        int propPosition = 2;
+        TrajectoryActionBuilder trajPropBuilder = drive.
+                actionBuilder(new Pose2d(12, 34, Math.toRadians(270)));
+        Action trajProp, reverseTrajProp;
+        switch(propPosition){
+            case 0: // left
+                trajProp = trajPropBuilder.turn(-Math.PI/2).build();
+                reverseTrajProp = drive.
+                        actionBuilder(new Pose2d(12, 34, Math.toRadians(180))).
+                        turn(Math.toRadians(-90)).build();
+                break;
+            case 1: // center
+                trajProp = trajPropBuilder.turn(Math.PI).build();
+                reverseTrajProp = drive.
+                        actionBuilder(new Pose2d(12, 34, Math.toRadians(90)))
+                        .turn(Math.toRadians(0)).build();
+                break;
+            case 2: // right
+                trajProp = trajPropBuilder.turn(Math.PI/2).build();
+                reverseTrajProp = drive.
+                        actionBuilder(new Pose2d(12, 34, 0.0)).
+                        turn(-Math.toRadians(-90)).build();
+                break;
+            default:
+                throw new Error("Unknown team prop position");
+        }
+        Action trajGoToBackdrop = drive.actionBuilder(new Pose2d(12, 34, Math.toRadians(90)))
+                .splineTo(new Vector2d(25, 47), 0)
+                .splineTo(new Vector2d(50, 33), 0)
+                .build();
+        Actions.runBlocking(new SequentialAction(
+                trajStart,
+                trajProp,
+
+                // servo action
+                reverseTrajProp,
+                trajGoToBackdrop
+        ));
+
+    }
+
+    private static void blueFar(MecanumDrive drive) {
+
+        Action trajStart = drive.actionBuilder(new Pose2d(-34, 60, Math.toRadians(270)))
+                .lineToY(34)
+//                .waitSeconds(0.4)
+//                .turn(Math.toRadians(-90))
+//                .lineToX(50)
+                .build();
+        int propPosition = 2;
+        TrajectoryActionBuilder trajPropBuilder = drive.
+                actionBuilder(new Pose2d(-34, 34, Math.toRadians(270)));
+        Action trajProp, reverseTrajProp;
+        switch(propPosition){
+            case 0: // left
+                trajProp = trajPropBuilder.turn(-Math.PI/2).build();
+                reverseTrajProp = drive.
+                        actionBuilder(new Pose2d(-34, 34, Math.toRadians(180))).
+                        turn(Math.toRadians(-90)).build();
+                break;
+            case 1: // center
+                trajProp = trajPropBuilder.turn(Math.PI).build();
+                reverseTrajProp = drive.
+                        actionBuilder(new Pose2d(-34, 34, Math.toRadians(90)))
+                        .turn(Math.toRadians(0)).build();
+                break;
+            case 2: // right
+                trajProp = trajPropBuilder.turn(Math.PI/2).build();
+                reverseTrajProp = drive.
+                        actionBuilder(new Pose2d(-34, 34, Math.PI)).
+                        turn(Math.toRadians(90)).build();
+                break;
+            default:
+                throw new Error("Unknown team prop position");
+        }
+        Action trajGoToBackdrop = drive.actionBuilder(new Pose2d(-34, 34, Math.toRadians(90)))
+                .splineTo(new Vector2d(-28, 60), 0)
+                .lineToX(25)
+                .splineTo(new Vector2d(50, 33), 0)
+                .build();
+        Actions.runBlocking(new SequentialAction(
+                trajStart,
+                trajProp,
+
+                // servo action
+                reverseTrajProp,
+                trajGoToBackdrop
+        ));
+
+    }
+
+    private static void redNear(MecanumDrive drive) {
+
+        Action trajStart = drive.actionBuilder(new Pose2d(12, -60, Math.toRadians(90)))
+                .lineToY(-34)
+//                .waitSeconds(0.4)
+//                .turn(Math.toRadians(-90))
+//                .lineToX(50)
+                .build();
+        int propPosition = 2;
+        TrajectoryActionBuilder trajPropBuilder = drive.
+                actionBuilder(new Pose2d(12, -34, Math.toRadians(90)));
+        Action trajProp, reverseTrajProp;
+        switch(propPosition){
+            case 0: // left
+                trajProp = trajPropBuilder.turn(-Math.PI/2).build();
+                reverseTrajProp = drive.
+                        actionBuilder(new Pose2d(12, -34, Math.toRadians(0))).
+                        turn(Math.toRadians(-90)).build();
+                break;
+            case 1: // center
+                trajProp = trajPropBuilder.turn(Math.PI).build();
+                reverseTrajProp = drive.
+                        actionBuilder(new Pose2d(12, 34, Math.toRadians(270)))
+                        .build();
+                break;
+            case 2: // right
+                trajProp = trajPropBuilder.turn(Math.PI/2).build();
+                reverseTrajProp = drive.
+                        actionBuilder(new Pose2d(12, -34, Math.PI)).
+                        turn(Math.toRadians(90)).build();
+                break;
+            default:
+                throw new Error("Unknown team prop position");
+        }
+        Action trajGoToBackdrop = drive.actionBuilder(new Pose2d(12, -34, Math.toRadians(270)))
+                .splineTo(new Vector2d(25, -47), 0)
+                .splineTo(new Vector2d(50, -33), 0)
+                .build();
+        Actions.runBlocking(new SequentialAction(
+                trajStart,
+                trajProp,
+
+                // servo action
+                reverseTrajProp,
+                trajGoToBackdrop
+        ));
+
+    }
+
+    private static void redFar(MecanumDrive drive) {
+
+        Action trajStart = drive.actionBuilder(new Pose2d(-34, -60, Math.toRadians(90)))
+                .lineToY(-34)
+                .build();
+        int propPosition = 2;
+        TrajectoryActionBuilder trajPropBuilder = drive.
+                actionBuilder(new Pose2d(-34, -34, Math.toRadians(90)));
+        Action trajProp, reverseTrajProp;
+        switch(propPosition){
+            case 0: // left
+                trajProp = trajPropBuilder.turn(-Math.PI/2).build();
+                reverseTrajProp = drive.
+                        actionBuilder(new Pose2d(-34, -34, Math.toRadians(0))).
+                        turn(Math.toRadians(-90)).build();
+                break;
+            case 1: // center
+                trajProp = trajPropBuilder.turn(Math.PI).build();
+                reverseTrajProp = drive.
+                        actionBuilder(new Pose2d(-34, -34, Math.toRadians(270)))
+                        .build();
+                break;
+            case 2: // right
+                trajProp = trajPropBuilder.turn(Math.PI/2).build();
+                reverseTrajProp = drive.
+                        actionBuilder(new Pose2d(-34, -34, Math.PI)).
+                        turn(Math.toRadians(90)).build();
+                break;
+            default:
+                throw new Error("Unknown team prop position");
+        }
+        Action trajGoToBackdrop = drive.actionBuilder(new Pose2d(-34, -34, Math.toRadians(270)))
+                .splineTo(new Vector2d(-28, -60), 0)
+                .lineToX(25)
+                .splineTo(new Vector2d(50, -33), 0)
+                .build();
+        Actions.runBlocking(new SequentialAction(
+                trajStart,
+                trajProp,
+
+                // servo action
+                reverseTrajProp,
+                trajGoToBackdrop
+        ));
+    }
+
 }
